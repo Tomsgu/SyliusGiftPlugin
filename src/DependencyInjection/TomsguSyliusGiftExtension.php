14037fccc63bd813c32a4b9924748579ff1f14e7
Tomas <tomasjakll@gmail.com>
2026-05-14 12:29:09 +0200

    chore: move to modern bundle layout

--- src/DependencyInjection/TomsguSyliusGiftExtension.php
+++ src/DependencyInjection/TomsguSyliusGiftExtension.php
@@ -7,9 +7,10 @@ namespace Tomsgu\SyliusGiftPlugin\DependencyInjection;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class TomsguSyliusGiftExtension extends AbstractResourceExtension
+final class TomsguSyliusGiftExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -17,10 +18,21 @@ final class TomsguSyliusGiftExtension extends AbstractResourceExtension
          * @psalm-suppress PossiblyNullArgument
          */
         $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
         $loader->load('services.xml');
 
         $this->registerResources('tomsgu_sylius_gift', $config['driver'], $config['resources'], $container);
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('framework', [
+            'validation' => [
+                'mapping' => [
+                    'paths' => [__DIR__ . '/../../config/validation'],
+                ],
+            ],
+        ]);
     }
 }
--- src/TomsguSyliusGiftPlugin.php
+++ src/TomsguSyliusGiftPlugin.php
@@ -12,10 +12,24 @@ final class TomsguSyliusGiftPlugin extends AbstractResourceBundle
 {
     use SyliusPluginTrait;
 
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
     public function getSupportedDrivers(): array
     {
         return [
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
         ];
+    }
+
+    protected function getConfigFilesPath(): string
+    {
+        return sprintf(
+            '%s/config/doctrine/%s',
+            $this->getPath(),
+            strtolower($this->getDoctrineMappingDirectory()),
+        );
     }
 }
